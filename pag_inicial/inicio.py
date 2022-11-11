b26382bebe0d490628bf4f56729b74aa58c4acc8
Laíssa Gabínio <89589831+Laissagabinio@users.noreply.github.com>
2022-11-11 14:58:28 -0300

    nao consegui testar o retangulo, mas se puder, agradeço

--- pag_inicial/inicio.py
+++ pag_inicial/inicio.py
@@ -19,27 +19,30 @@ pygame.display.set_caption('Jogo de Damas')
 janela = pygame.display.set_mode((960,720))
 janela_aberta = True
 
+pygame.draw.rect(janela, VERMELHO_ESCURO, pygame.Rect(30, 30, 60, 60),  2, 3)
+
 
 #padrão dos textos
-def textos(text, font, color):
-	textSurface = font.render(text, True, color)
+def textos(texto, font, color):
+	textSurface = font.render(texto, True, color)
 	return textSurface, textSurface.get_rect()
 
 #CRIAR BOTÃO
-def criar_botao(texto, elipse, cor1, cor2, BRANCO, acao=None):
+def criar_botao(texto, retangulo, VERMELHO_ESCURO, VERMELHOR_SOMBRA, BRANCO, acao=None):
 	mouse = pygame.mouse.get_pos()
 	clique = pygame.mouse.get_pressed()
 
-	if elipse[0] + elipse[2] > mouse[0] > elipse[0] and elipse[1] + elipse[3] > mouse[1] > elipse[1]:
-		pygame.draw.rect(janela, cor2, elipse)
+
+	if retangulo[0] + retangulo[2] > mouse[0] > retangulo[0] and retangulo[1] + retangulo[3] > mouse[1] > retangulo[1]:
+		pygame.draw.rect(janela, VERMELHO_ESCURO, retangulo)
 		if clique[0] == 1 and acao != None:
 			acao()
 	else:
-		pygame.draw.rect(janela, cor1, elipse)
+		pygame.draw.rect(janela, VERMELHO_SOMBRA, retangulo)
 
 	fontePequena = pygame.font.SysFont('comicsansms', 20)
 	surface_texto, rect_texto = textos(texto, fontePequena, BRANCO)
-	rect_texto.center = (elipse[0] + 60, elipse[1] + 20)
+	rect_texto.center = (retangulo[0] + 60, retangulo[1] + 20)
 	janela.blit(surface_texto, rect_texto)
 
 
